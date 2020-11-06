14e668ff81710648696d1c98ec95b605b46e6456
SimonRua <simon.ruavargas.contractor@bydeluxe.com>
2020-11-05 21:26:07 -0500

    removing login component import

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignupComponent } from './signup/signup.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    SignupComponent,
-    LoginComponent
+    SignupComponent
   ],
   imports: [
     CommonModule,
