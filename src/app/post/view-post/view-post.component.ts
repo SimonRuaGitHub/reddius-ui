108be97d5575231566a573ce5c0aa891ba9e094f
SimonRuaGitHub <simonfruavargas@gmail.com>
2021-01-20 22:29:32 -0500

    adding filter condition for logged in when creating comment and fixing alignation issue in vote button component

--- src/app/post/view-post/view-post.component.ts
+++ src/app/post/view-post/view-post.component.ts
@@ -5,6 +5,7 @@ import { PostService } from 'src/app/services/post.service';
 import { PostModel } from 'src/app/shared/post-model';
 import { faCommentAlt } from '@fortawesome/free-solid-svg-icons'
 import { AuthStorageService } from 'src/app/services/storage/auth-storage.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-view-post',
@@ -17,15 +18,17 @@ export class ViewPostComponent implements OnInit {
   post$: PostModel;
   username: string;
   readonly faCommentAlt = faCommentAlt;
+  isLoggedIn:boolean;
 
-  constructor(private postService:PostService, private activateRoute:ActivatedRoute) { 
+  constructor(private postService:PostService, private activateRoute:ActivatedRoute, private authService:AuthService) { 
      this.postId = this.activateRoute.snapshot.params.id;
   }
 
   ngOnInit(): void {
      this.postService.getPost(this.postId).subscribe(post => this.post$ = post, 
                                                      error  => throwError(error), 
-                                                    () => console.log("request to getPost executed"))
+                                                    () => console.log("request to getPost executed"));
+      this.isLoggedIn = this.authService.isLoggedIn();                                               
   }
 
 }
